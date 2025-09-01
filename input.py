2cf013eb5a31d7363960a271261a83e63781efd5
Erik_Barboza <erikbarboza07@gmail.com>
2025-09-01 17:00:44 +0000

    conversão e input

---
+++ input.py
@@ -0,0 +1,7 @@
+N1 = float(input("digite a primeira nota: "))
+N2 = float(input("digite a segunda nota: "))
+N3 = float(input("digite a terceira nota: "))
+
+media = (N1 + N2 + N3) / 3
+
+print(f"Sua média é igual a: {media:.2f}")
---
+++ teste1.py
@@ -0,0 +1,16 @@
+#variaveis de identificação de individuo
+
+nome = "Ana" 
+idade = 22
+cidade = "Belo Horizonte"
+profissao = "estudante"
+ 
+ #identificação de individuos para o console
+ 
+print("nome: ", nome)
+print("idade: ", idade)
+print("cidade: ", cidade)
+print("Profissão: ", profissao)
+ 
+if profissao == "estudante":
+    print(profissao, "? ...Isso nem é profissão, procure um trabalho de verdade")
